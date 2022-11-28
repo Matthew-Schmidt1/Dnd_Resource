2b81018e1c59297837a97157c56dc5e41399ce56
Matthew-Schmidt1 <Matthew_Schmidt@outlook.com>
2022-11-28 21:10:32 +1300

    hope full the last please

--- myScript.js
+++ myScript.js
@@ -10,17 +10,3 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.visibility === "visible") {
-      content.style.visibility = "hidden";
-    } else {
-      content.style.visibility = "visible";
-    }
-  });
-}
