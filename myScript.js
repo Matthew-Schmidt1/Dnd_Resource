d422173b108f03661090f186dbecb93f32533c5b
Matthew-Schmidt1 <Matthew_Schmidt@outlook.com>
2022-11-28 21:03:45 +1300

    please just work

--- myScript.js
+++ myScript.js
@@ -17,10 +17,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+    if (content.style.visibility === "visible") {
+      content.style.visibility = "hidden";
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.visibility = "visible";
     }
   });
 }
