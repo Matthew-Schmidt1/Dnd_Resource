d7b6515ca3d14a18a2e788de2aa83e65be1de023
Matthew-Schmidt1 <Matthew_Schmidt@outlook.com>
2022-11-28 20:37:34 +1300

    got told no

--- myScript.js
+++ myScript.js
@@ -14,7 +14,7 @@ function openNav() {
     var coll = document.getElementsByClassName("collapsible");
     var i;
     
-    for (i = 0; i <= coll.length; i++) {
+    for (i = 0; i < coll.length; i++) {
       coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
