f732d4c10ac05cad5b1e6dc5590d154f1cd02f52
Matthew-Schmidt1 <Matthew_Schmidt@outlook.com>
2022-11-28 20:30:25 +1300

    lets try this

--- myScript.js
+++ myScript.js
@@ -10,8 +10,8 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "0";
   }
 
-  function expand(section){
-    var coll = document.getElementById(section);
+  function expand(){
+    var coll = document.getElementsByClassName("collapsible");
     var i;
     
     for (i = 0; i < coll.length; i++) {
