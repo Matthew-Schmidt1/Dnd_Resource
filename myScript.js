293dc9dac00d5bb54b096f2bc1ed7804079a2cba
Matthew-Schmidt1 <Matthew_Schmidt@outlook.com>
2022-11-29 12:19:07 +1300

    style update

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "500px";
+    document.getElementById("mySidenav").style.width = "25%";
+    document.getElementById("main").style.marginLeft = document.getElementById("mySidenav").style.width;
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
